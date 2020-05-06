34d5f4367f740cdca65c1b45837558d563b90d78
Gregory Ling <0b0100011101101100@gmail.com>
2020-05-06 13:43:42 -0700

    DFLL 48MHz clock working

--- sources/project1/blinky.c
+++ sources/project1/blinky.c
@@ -1,6 +1,8 @@
 #include <sam.h>
+#include "nvm_data.h"
+
 #define PORTA PORT->Group[0]
-#define PORTB PORT->Group[0]
+#define PORTB PORT->Group[1]
 
 enum {
     SPI_MOSI_0_SCK_1 = 0,
@@ -22,72 +24,64 @@ enum{
 
 int main()
 {
-    NVMCTRL->CTRLB.reg = NVMCTRL_CTRLB_RWS_HALF;
+    NVMCTRL->CTRLB.bit.RWS = 1;
     // keep at 1MHz for DPLL
-    SYSCTRL->OSC8M.bit.PRESC = 3; // Reset the core clock to 8MHz... It's reset to 1MHz on reset
+    //SYSCTRL->OSC8M.bit.PRESC = 3; // Reset the core clock to 8MHz... It's reset to 1MHz on reset
     PORTA.DIRSET.reg = PORT_PA06 | PORT_PA20 | PORT_PA22 | PORT_PA23; // Set output pins
 
     PM->APBCMASK.reg = PM_APBCMASK_SERCOM5; // Enable SERCOM5
 
-    GCLK->GENDIV.reg = GCLK_GENDIV_ID()
-    
-    SYSCTRL->OSC32K.reg =
+    SYSCTRL->DFLLCTRL.reg = 0; // See Errata 9905, yep, it's necessary for DFLL to work
+    while ((SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY) == 0)
+        ; // Wait for DFLL synchronization complete
+
+    /*SYSCTRL->OSC32K.reg =
         SYSCTRL_OSC32K_STARTUP(0x6) |
         SYSCTRL_OSC32K_EN32K |
+        SYSCTRL_OSC32K_CALIB(NVM_READ_CAL(OSC32K_CAL)) |
         SYSCTRL_OSC32K_ENABLE;
 
-    // enable generator 4 from OSC8M
     GCLK->GENCTRL.reg =
+        GCLK_GENCTRL_OE |
         GCLK_GENCTRL_GENEN |
-        GCLK_GENCTRL_SRC_OSC32K |
-        GCLK_GENCTRL_ID(2); // PA20
+        GCLK_GENCTRL_SRC_OSC8M |
+        GCLK_GENCTRL_ID(2);
 
-    // Set DFLL reference to GCLK2
+    while (
+        GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY || 
+        !(SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_OSC32KRDY)); // Wait for sync
+
     GCLK->CLKCTRL.reg =
         GCLK_CLKCTRL_CLKEN |
         GCLK_CLKCTRL_GEN_GCLK2 |
         GCLK_CLKCTRL_ID_DFLL48;
 
-    // From Adafruit source code...
-    #define NVM_SW_CALIB_DFLL48M_COARSE_VAL 58
+    while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY); // Wait for sync
 
-    // Turn on DFLL
-    uint32_t coarse = (*((uint32_t *)(NVMCTRL_OTP4) + (NVM_SW_CALIB_DFLL48M_COARSE_VAL / 32)) >> (NVM_SW_CALIB_DFLL48M_COARSE_VAL % 32)) & ((1 << 6) - 1);
-    if (coarse == 0x3f)
-    {
-        coarse = 0x1f;
-    }
-    // TODO(tannewt): Load this value from memory we've written previously. There
-    // isn't a value from the Atmel factory.
-    uint32_t fine = 0x1ff;
+    SYSCTRL->DFLLCTRL.reg =
+        SYSCTRL_DFLLCTRL_BPLCKC;
 
-    SYSCTRL->DFLLVAL.bit.COARSE = coarse;
-    SYSCTRL->DFLLVAL.bit.FINE = fine;
-    /* Write full configuration to DFLL control register */
-    /*SYSCTRL->DFLLMUL.reg = SYSCTRL_DFLLMUL_CSTEP(0x1f / 4) | // Coarse step is 31, half of the max value
-                           SYSCTRL_DFLLMUL_FSTEP(10) |
-                           SYSCTRL_DFLLMUL_MUL((48000));
+    SYSCTRL->DFLLMUL.reg =
+        SYSCTRL_DFLLMUL_CSTEP(0x1f / 4) |
+        SYSCTRL_DFLLMUL_FSTEP(10) |
+        SYSCTRL_DFLLMUL_MUL(48);*/
 
-    // End Adafruit*/
+    SYSCTRL->DFLLVAL.reg =
+        SYSCTRL_DFLLVAL_COARSE(NVM_READ_CAL(DFLL48M_COARSE_CAL)) |
+        SYSCTRL_DFLLVAL_FINE(NVM_READ_CAL(DFLL48M_FINE_CAL));
 
-    // Enable DFLL (faster clock)
-    /*SYSCTRL->DFLLCTRL.reg =
-        SYSCTRL_DFLLCTRL_MODE |
-        //SYSCTRL_DFLLCTRL_CCDIS |
-        //SYSCTRL_DFLLCTRL_USBCRM |
-        //SYSCTRL_DFLLCTRL_BPLCKC |
+    SYSCTRL->DFLLCTRL.reg =
+        //SYSCTRL_DFLLCTRL_MODE |
         SYSCTRL_DFLLCTRL_ENABLE;
 
+    while (!(SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY));
 
-    // And set generator 4 from it with output
     GCLK->GENCTRL.reg =
         GCLK_GENCTRL_OE |
-        GCLK_GENCTRL_GENEN |
         GCLK_GENCTRL_SRC_DFLL48M |
-        GCLK_GENCTRL_ID(4);*/
-   
+        GCLK_GENCTRL_ID(4) |
+        GCLK_GENCTRL_GENEN;
 
-    
     PORTA.PINCFG[20].reg = PORT_PINCFG_PMUXEN | PORT_PINCFG_DRVSTR;
     PORTA.PMUX[10].reg = PORT_PMUX_PMUXE_H | PORT_PMUX_PMUXO_H;
     
